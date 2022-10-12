293581f5d0806f0c0d0162cf410ee16adc5fcb79
CODE\Kanan.dA <kenelisan@gmail.com>
2022-10-12 13:57:33 +0400

    Change bullet destroy with distance with player

--- CA2DPlatformerPractice/Assets/Scripts/Bullet.cs
+++ CA2DPlatformerPractice/Assets/Scripts/Bullet.cs
@@ -7,25 +7,28 @@ public class Bullet : MonoBehaviour
     [SerializeField] int BulletDamage = 50;
     Rigidbody2D rb;
     [SerializeField] float BulletForce = 5f;
+    GameObject _Player;
     PlayerController PlayerScript;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        PlayerScript = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>();
+        _Player = GameObject.FindGameObjectWithTag("Player");
+        PlayerScript = _Player.GetComponent<PlayerController>();
 
         if (PlayerScript.FireDirection == true) {
             rb.AddForce(Vector2.right * BulletForce, ForceMode2D.Impulse);
         }else {
             rb.AddForce(Vector2.left * BulletForce, ForceMode2D.Impulse);
         }
-        Destroy(gameObject, 3);
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        if (Vector3.Distance(transform.position, _Player.transform.position) >= 12) {
+            Destroy(gameObject);
+        }
     }
 
     private void OnCollisionEnter2D(Collision2D other) {
